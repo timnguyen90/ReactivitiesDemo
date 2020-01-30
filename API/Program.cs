ed01da6d8abf2e9afa58e66d6389a6dc533f1379
Tim Nguyen <longdt80@gmail.com>
2020-01-31 02:31:18 +0700

    End of Section 4

--- API/Program.cs
+++ API/Program.cs
@@ -1,10 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -17,18 +13,19 @@ namespace API
         public static void Main(string[] args)
         {
             var host = CreateHostBuilder(args).Build();
-            using(var scope = host.Services.CreateScope())
+            using (var scope = host.Services.CreateScope())
             {
                 var services = scope.ServiceProvider;
                 try
                 {
                     var context = services.GetRequiredService<DataContext>();
                     context.Database.Migrate();
+                    Seed.SeedData(context);
                 }
-                catch(Exception ex)
+                catch (Exception ex)
                 {
                     var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex,"An error occured during migration");
+                    logger.LogError(ex, "An error occured during migration");
                 }
             }
             host.Run();
--- API/Startup.cs
+++ API/Startup.cs
@@ -1,16 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Application.Activities;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Persistence;
 
 namespace API
@@ -25,20 +20,24 @@ namespace API
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDbContext<DataContext>(opt=>{
-                opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
-            });
-            // Begin of Adding CORS Support to the API
-            services.AddCors(opt=>{
-                opt.AddPolicy("CorsPolicy",policy=>{
-                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
-                });
-            });
-            // End of Adding CORS Support to the API
-            services.AddControllers();
-        }
+public void ConfigureServices(IServiceCollection services)
+{
+    services.AddDbContext<DataContext>(opt=>{
+        opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
+    });
+    // Begin of Adding CORS Support to the API
+    services.AddCors(opt=>{
+        opt.AddPolicy("CorsPolicy",policy=>{
+            policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+        });
+    });
+    // End of Adding CORS Support to the API
+
+    // Adding mediatR
+    services.AddMediatR(typeof(List.Handler).Assembly);
+
+    services.AddControllers();
+}
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
